76cbb002e3248e225a8d425b1e76509a6059d22a
Saup21 <sauparna.gupta@gmail.com>
2021-05-28 18:08:14 +0530

    Essential files added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# from .forms import RegistrationForm, PostcreationForm
+# from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+
+def blog(request):
+    return render(request, 'blog/post_list.html', {})
