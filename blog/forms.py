eb2e4096bf6bb2101e30d64dd32135ac9914d4ff
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-29 03:54:10 +0530

    comment model and ajax view

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 # Necessary forms:
 
@@ -8,3 +8,14 @@ class PostcreationForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text', 'image']
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput(attrs={'type':'text','name':'comment','class':'form-control','id':'comment','placeholder':'Comment....'})
